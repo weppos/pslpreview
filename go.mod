62cf90b6796f05da1dedf7d659a6d432d44cfcc5
Simone Carletti <weppos@weppos.net>
2020-02-26 23:20:34 +0100

    Parse given Hosts against default PSL and return results

---
+++ go.mod
@@ -0,0 +1,10 @@
+// +heroku goVersion go1.13
+
+module github.com/weppos/pslpreview
+
+go 1.12
+
+require (
+	github.com/weppos/publicsuffix-go v0.13.0
+	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b // indirect
+)
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/weppos/publicsuffix-go/publicsuffix"
 )
 
 func main() {
@@ -25,11 +27,11 @@ func PreviewServer(w http.ResponseWriter, r *http.Request) {
 	var value string
 	pp := &PreviewParams{}
 
-	if value = query.Get("r"); value == "" {
-		fmt.Fprintf(w, "Parameter 'r' is missing")
-		return
-	}
-	pp.setRules(value)
+	//if value = query.Get("r"); value == "" {
+	//	fmt.Fprintf(w, "Parameter 'r' is missing")
+	//	return
+	//}
+	//pp.setRules(value)
 
 	if value = query.Get("h"); value == "" {
 		fmt.Fprintf(w, "Parameter 'h' is missing")
@@ -37,7 +39,9 @@ func PreviewServer(w http.ResponseWriter, r *http.Request) {
 	}
 	pp.setHosts(value)
 
-	fmt.Fprintf(w, fmt.Sprintf("%v", pp))
+	prs := preview(pp)
+
+	fmt.Fprintf(w, fmt.Sprintf("%v", prs))
 }
 
 type PreviewParams struct {
@@ -53,4 +57,57 @@ func (p *PreviewParams) setRules(value string) error {
 func (p *PreviewParams) setHosts(value string) error {
 	p.Hosts = strings.Split(value, ",")
 	return nil
+}
+
+type PreviewResults struct {
+	Results []PreviewResult
+}
+
+type PreviewResult struct {
+	Host   string
+	Domain PreviewDomain
+	Error  error
+}
+
+type PreviewDomain struct {
+	ETLD        string
+	ETLDPlusOne string
+	Rule string
+}
+
+func newPreviewDomainFromPublicSuffix(d *publicsuffix.DomainName) *PreviewDomain {
+	if d == nil {
+		return nil
+	}
+
+	pd := &PreviewDomain{}
+	pd.ETLD = d.TLD
+	pd.ETLDPlusOne = d.SLD + "." + d.TLD
+
+	rule := d.Rule.Value
+	if d.Rule.Type == publicsuffix.WildcardType {
+		rule = "*." + rule
+	}
+	if d.Rule.Type == publicsuffix.ExceptionType {
+		rule = "!" + rule
+	}
+	pd.Rule = rule
+
+	return pd
+}
+
+func preview(pp *PreviewParams) *PreviewResults {
+	prs := &PreviewResults{}
+
+	for _, h := range pp.Hosts {
+		pr := PreviewResult{Host: h}
+
+		d, err := publicsuffix.Parse(pr.Host)
+		pr.Domain = *newPreviewDomainFromPublicSuffix(d)
+		pr.Error = err
+
+		prs.Results = append(prs.Results, pr)
+	}
+
+	return prs
 }
